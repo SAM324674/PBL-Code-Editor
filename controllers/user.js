fc73ade55ad8d1e87d5d76f3587f1d97536fb301
ft.souptik <mr.souptiktaran@gmail.com>
2024-04-23 11:17:07 +0530

    v1.1.1 : User Authentification added

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,43 @@
-module.exports.Signin = async (req , res)=>{
+const { userSchema } = require('../schemas/userValidator')
+const User = require('../models/user')
+
+
+module.exports.Signin = async (req, res) => {
     res.send('Signin Page');
 }
 
-module.exports.SignUser = async(req , res)=>{
-    res.json({msg : 'This is where the sigin logic is hold'})
+module.exports.SignUser = async (req, res) => {
+    const { username, email, password } = req.body
+    const data = {
+        username,
+        email,
+        password
+    }
+
+    //checking the input type of the given data
+    let checking = userSchema.safeParse(data)
+    if (!checking.success) {
+        return res.status(400).json({ msg: 'Bad Parameters' })
+    }
+    await User.create({
+        username,
+        email,
+        password
+    });
+    return res.status(200).redirect("/")
+
 }
 
-module.exports.Login = async (req ,res)=>{
+module.exports.Login = async (req, res) => {
     res.send('Login Page')
 }
 
-module.exports.LoginUser = async(req , res)=>{
-    res.json({msg : 'This is where the Login logic is hold'})
-
+module.exports.LoginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await User.matchPasswordandGenerateToken(email, password);
+        return res.cookie("token", token).redirect("/");
+    } catch (error) {
+        return res.json({ error: "Incorrect Email or Password" });
+    }
 }
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express();
 const PORT = 8000
+const colors = require('colors');
+const PATH = require('path')
 const userRoutes = require('./routes/user')
+
+//setting ejs 
+app.set('view engine' , 'ejs')
+app.set('views' , PATH.resolve('./views'))
+
+
+//Database Connection
+const db = require('./configs/mongoose')
 
 //middlewares
 app.use(express.json())
@@ -9,8 +19,11 @@ app.use(express.json())
 //Routes
 app.use('/user' , userRoutes);
 
+app.get('/' , (req , res)=>{
+    res.render('home')
+})
 
 
 app.listen(PORT , ()=>{
-    console.log(`SERVER STARTED : http://localhost:${PORT}/`)
+    console.log(`SERVER STARTED : http://localhost:${PORT}/`.bgGreen.black)
 })
